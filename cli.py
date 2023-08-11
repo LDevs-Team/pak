5a50f3ce5532c10510628380ddbf9edd09a9b5ba
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2023-08-11 10:29:03 +0200

    feat!: added updates and uninstallation (beta)

--- cli.py
+++ cli.py
@@ -7,5 +7,9 @@ PACKAGE = sys.argv[2]
 match METHOD.lower():
     case "install":
         core.install(PACKAGE)
+    case "uninstall":
+        core.uninstall(PACKAGE)
+    case "upgrade":
+        core.upgrade(PACKAGE)
     case _:
         raise NotImplementedError("Other functionality is still not implemented")
--- core.py
+++ core.py
@@ -31,15 +31,14 @@ def unpack(filePath: str) -> dict:
 
 
 def webDownloadPackage(package_name:str) -> str:
-    
     with requests.get(f"{BASE_URL}{package_name}.zip") as r:
         if r.status_code != 200:
             raise ValueError('The specified package does not exist')
         with open(f"{package_name}.zip", "wb") as f:
             f.write(r.content)
     return f"{package_name}.zip"
-        
-def updatePackagesDatabase(packageName:str, installPath:str) -> None:
+
+def initDatabase() -> tuple[sqlite3.Connection, sqlite3.Cursor]:
     db_path = ''
     match platform.system():
         case 'Windows':
@@ -65,32 +64,90 @@ def updatePackagesDatabase(packageName:str, installPath:str) -> None:
     cursor = db.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS PACKAGES (NAME TEXT, INSTALLPATH TEXT)")
     db.commit()
+    return db, cursor
+    
+def queryDatabase(packageName:str) -> list[tuple[str, str]] | None:
+    db, cursor = initDatabase()
+    cursor.execute("SELECT * FROM PACKAGES WHERE NAME = ?", (packageName,))
+    return cursor.fetchall()
+
+def updatePackagesDatabase(packageName:str, installPath:str) -> None:
+    db, cursor = initDatabase()
     cursor.execute("INSERT INTO PACKAGES (NAME, INSTALLPATH) VALUES (?, ?)", (packageName, installPath))
     db.commit()
+
+def removeFromPackagesDatabase(packageName:str) -> None:
+    db, cursor = initDatabase()
+    cursor.execute("DELETE FROM PACKAGES WHERE NAME = (?)", (packageName,))
+    db.commit()
     
+def executeManifestKey(key: dict, name:str, packageName):
+    match key['adapter']:
+        case 'pak/adapter-ps1':
+            if adapters.adapter_ps1(os.path.abspath(key['file']), key['needsReboot']):
+                print("Success!")
+                return
+        case 'pak/adapter-batch':
+            if adapters.adapter_batch(os.path.abspath(key['file']), key['needsReboot']):
+                print("Success!")
+                return                  
+        case _:
+            raise classes.BadPackage('Adapter for "install" is invalid')
+    print("It looks like there was an error while installing the package. :(")
+    return
         
 def install(package: str):
     if os.path.exists(package):
         if os.path.isfile(package):
+            package = os.path.abspath(package)
             manifest = unpack(package)
         else:
             raise ValueError('Specified file is not a package')
     else:
         manifest = unpack(webDownloadPackage(package))
-        print(f"Starting installation of {manifest['name']}")
-        install_key = manifest['installation']
-        match install_key['adapter']:
-            case 'pak/adapter-ps1':
-                if adapters.adapter_ps1(os.path.abspath(install_key['file']), install_key['needsReboot']):
-                    print("Package installed successfully!")
-                    updatePackagesDatabase(manifest['name'], package)
-                    return
-            case 'pak/adapter-batch':
-                if adapters.adapter_batch(os.path.abspath(install_key['file']), install_key['needsReboot']):
-                    print("Package installed successfully!")
-                    updatePackagesDatabase(manifest['name'], package)
-                    return                  
-            case _:
-                raise classes.BadPackage('Adapter for "install" is invalid')
-        print("It looks like there was an error while installing the package. :(")
-    return
+    
+    if len(queryDatabase(manifest['name'])) > 0:
+        print("Package is already installed.")
+        return
+    
+    print(f"Starting installation of {manifest['name']}")
+    install_key = manifest['installation']
+    executeManifestKey(install_key, manifest['name'], package)
+    updatePackagesDatabase(manifest['name'], package)
+
+def uninstall(package: str):
+    if os.path.exists(package):
+        if os.path.isfile(package):
+            package = os.path.abspath(package)
+            manifest = unpack(package)
+        else:
+            raise ValueError('Specified file is not a package')
+    else:
+        manifest = unpack(webDownloadPackage(package))
+    
+    if len(queryDatabase(manifest['name'])) == 0:
+        print("Package is not installed.")
+        return
+    
+    print(f"Starting uninstallation of {manifest['name']}")
+    uninstall_key = manifest['uninstallation']
+    executeManifestKey(uninstall_key, manifest['name'], package)
+    removeFromPackagesDatabase(manifest['name'])
+    
+def upgrade(package: str):
+    if os.path.exists(package):
+        if os.path.isfile(package):
+            package = os.path.abspath(package)
+            manifest = unpack(package)
+        else:
+            raise ValueError('Specified file is not a package')
+    else:
+        manifest = unpack(webDownloadPackage(package))
+    
+    if len(queryDatabase(manifest['name'])) == 0:
+        print("Package is not installed.")
+        return
+    
+    print(f"Starting upgrade of {manifest['name']}")
+    upgrade_key = manifest['update']
+    executeManifestKey(upgrade_key, manifest['name'], package)
