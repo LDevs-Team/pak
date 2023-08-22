c3012b06224113118263a2390910509a1c510ba9
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2023-08-22 11:09:27 +0200

    chore: added docstrings

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+from typing import Dict, Union
+from classes import BadPackage
 import os
 import zipfile
 import json
@@ -11,16 +13,33 @@ import platform
 import typing
 from urllib.parse import urlparse
 from pathlib import Path
-from database import initDatabase, queryDatabase, updatePackagesDatabase, removeFromPackagesDatabase
+from database import queryDatabase, updatePackagesDatabase, removeFromPackagesDatabase
 
 BASE_URL = "https://github.com/LDevs-Team/pak-pkgs/raw/main/"
 
 def init_temp():
+    """
+    Initializes a temporary directory.
+
+    This function checks if the 'Temp' directory exists. If it does, the directory is removed using the 'shutil.rmtree()' function. Then, a new 'Temp' directory is created using the 'os.mkdir()' function.
+
+    Parameters:
+        - None
+
+    Returns:
+        - None
+    """
     if os.path.exists('Temp'):
         shutil.rmtree('Temp')
     os.mkdir("Temp")
 
 def init_pkgs():
+    """
+    Initializes the packages by determining the appropriate path for the manifests file based on the current platform.
+
+    Returns:
+        str: The path to the manifests file.
+    """
     manifests_path = ''
     match platform.system():
         case 'Windows':
@@ -38,24 +57,47 @@ def init_pkgs():
     if not os.path.exists(manifests_path):
         Path.mkdir(manifests_path, exist_ok=True, parents=True)
     return manifests_path
-def unpack(filePath: str) -> dict|classes.BadPackage:
-    """returns the manifest from a zip file"""
+
+def unpack(file_path: str) -> Union[Dict, BadPackage]:
+    """
+    Unpacks a zip file and returns the manifest as a dictionary.
+    
+    Args:
+        file_path (str): The path to the zip file.
+    
+    Returns:
+        Union[Dict, BadPackage]: The manifest dictionary if it exists, 
+                                otherwise raises a BadPackage exception.
+    """
     try:
-        with zipfile.ZipFile(filePath) as zip:
-            zip.extractall('Temp')
+        with zipfile.ZipFile(file_path) as zip_file:
+            zip_file.extractall('Temp')
     except zipfile.BadZipFile:
-        raise classes.BadPackage("Package is not a zipfile")
+        raise BadPackage("Package is not a zipfile")
     
     os.chdir("Temp")
     try:
-        with open('manifest.json', 'r') as f:
-            manifest:dict = json.load(f)
+        with open('manifest.json', 'r') as manifest_file:
+            manifest = json.load(manifest_file)
             return manifest
     except OSError:
-        raise classes.BadPackage("Package has no manifest.json file")
+        raise BadPackage("Package has no manifest.json file")
 
 
 def webDownload(url:str, filename:str) -> str:
+    """
+    Downloads a file from the specified URL and saves it to the specified filename.
+
+    Parameters:
+    - url (str): The URL of the file to be downloaded.
+    - filename (str): The name of the file to be saved.
+
+    Returns:
+    - str: The path of the downloaded file.
+
+    Raises:
+    - ValueError: If the specified package does not exist.
+    """
     init_temp()
     with requests.get(url) as r:
         if r.status_code != 200:
@@ -66,6 +108,19 @@ def webDownload(url:str, filename:str) -> str:
     return f"Temp/{filename}"
     
 def executeManifestKey(key: dict, operationType:str):
+    """
+    Executes a manifest key based on the given parameters.
+
+    Parameters:
+    - key (dict): A dictionary representing the manifest key.
+    - operationType (str): The type of operation to be performed.
+
+    Returns:
+    - None: If the adapter is successfully executed.
+    - MissingFileError: If the file specified in the manifest key does not exist.
+    - BadPackage: If the adapter for the "install" operation is invalid.
+    - PackageUpdateError: If there is an error during the package update operation.
+    """
     init_temp()
     fileToExecute = key['file']
     if key['file'].startswith('url:'):
@@ -90,6 +145,18 @@ def executeManifestKey(key: dict, operationType:str):
     raise classes.PackageUpdateError(operationType)
 
 def downloadPackageFromRepo(package):
+    """
+    Downloads a package from the repository.
+
+    Parameters:
+        package (str): The name of the package to download.
+
+    Returns:
+        str: The path to the downloaded package.
+
+    Raises:
+        ValueError: If the specified package does not exist.
+    """
     try:
         return webDownload(f"{BASE_URL}{package}.zip", f"{package}.zip")
     except ValueError:
@@ -99,6 +166,18 @@ def downloadPackageFromRepo(package):
     
 
 def install(package: str):
+    """
+    Installs a package.
+
+    Args:
+        package (str): The name or path of the package to install.
+
+    Raises:
+        ValueError: If the specified file is not a package.
+
+    Returns:
+        None
+    """
     manifests_path = init_pkgs()
     if os.path.exists(package):
         if os.path.isfile(package):
@@ -134,7 +213,18 @@ def install(package: str):
     updatePackagesDatabase(manifest['name'], package, manifests_path+"/"+os.path.basename(os.path.realpath(packageFile)))
 
 def uninstall(package: str):
-    db, cursor = initDatabase()
+    """
+    Uninstalls a package.
+
+    Parameters:
+        package (str): The name of the package to uninstall.
+
+    Raises:
+        ValueError: If the package is not installed.
+
+    Returns:
+        None
+    """
     result = queryDatabase(package)
     if len(result) < 1:
         raise ValueError("Package is not installed")
@@ -151,6 +241,15 @@ def uninstall(package: str):
     removeFromPackagesDatabase(manifest['name'])
     
 def upgrade(package: str):
+    """
+    Upgrade a package.
+
+    Parameters:
+        package (str): The name or path of the package to upgrade.
+
+    Returns:
+        None
+    """
     if os.path.exists(package):
         if os.path.isfile(package):
             package = os.path.abspath(package)
